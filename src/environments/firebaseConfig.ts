d784942384aff42b0b2f5055c7b03fd3ee41564c
Daniel Fidalgo <daniel.fidalgo@gmail.com>
2017-09-17 14:14:30 -0300

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyBz-rcQadmgtQ8ZtqqhvZAs3AAK6-2LTrc",
+    authDomain: "cruzvermelhadf.firebaseapp.com",
+    databaseURL: "https://cruzvermelhadf.firebaseio.com/",
+    storageBucket: "gs://cruzvermelhadf.appspot.com/",
+    messagingSenderId: "935358902590"
+};
+
